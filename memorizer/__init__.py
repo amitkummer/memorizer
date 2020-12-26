a7a2cfad4ee9704fa9b45e0b342eb11c0331dce6
amitkummer <amit.kummer@gmail.com>
2020-12-26 14:39:59 +0200

    Add initial working CLI

---
+++ memorizer/__init__.py
@@ -0,0 +1,32 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import random
+import json
+import fileinput
+
+def run():
+    parser = ArgumentParser()
+    parser.add_argument('--path', type=Path)
+    parser.add_argument('--count', type=int)
+    args = parser.parse_args()
+    sampledWords = sampleWordsFromFile(args.path, args.count)
+    startGame(sampledWords)
+
+def sampleWordsFromFile(path, sampleCount):
+    with path.open() as wordsFile:
+        words = json.load(wordsFile)
+        return random.sample(words, sampleCount)
+
+def startGame(words):
+    print("Game is starting! Write your answer and press enter.")
+    console = fileinput.input('-')
+    for word in words:
+        german = word["word"]
+        meaning = word["meaning"]
+        print(f"What is the English word for the word: {german}?")
+        answer = console.readline()
+        answer = answer.lower().strip()
+        if (answer == meaning.strip().lower()):
+            print("Your'e answer is correct!")
+        else:
+            print(f"Wrong! The correct answer is: {meaning}")
