1ef5eca1012d3db18cd0c2d1277acca55cc934eb
amitkummer <amit.kummer@gmail.com>
2020-12-26 18:57:03 +0200

    Update readme with instructions

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
-# Running The Project
+# Example Usage
+
+First download the dependencies using poetry:
 
 ```sh
-poetry run memorizer
+pip install poetry
+poetry install
+```
+
+Then run the CLI:
+
+```sh
+poetry run memorizer --path data/test.json --count 3
+```
+
+# Running The Tests
+
+Spawn a shell inside the poetry environment, then call pytest:
+
+```sh
+poetry shell
+pytest
 ```
--- memorizer/__init__.py
+++ memorizer/__init__.py
@@ -5,7 +5,7 @@ import json
 import fileinput
 
 def run():
-    parser = ArgumentParser()
+    parser = ArgumentParser(prog='memorizer')
     parser.add_argument('--path', type=Path)
     parser.add_argument('--count', type=int)
     args = parser.parse_args()
@@ -18,17 +18,17 @@ def sampleWordsFromFile(path, sampleCount):
         return random.sample(words, sampleCount)
 
 def startGame(words):
-    print("Game is starting! Write your answer and press enter.")
+    print('Game is starting! Write your answer and press enter.')
     console = fileinput.input('-')
     for word in words:
-        german = word["word"]
-        meaning = word["meaning"]
-        print(f"What is the English word for the word: {german}?")
+        german = word['word']
+        meaning = word['meaning']
+        print(f'What is the English word for the word: {german}?')
         answer = console.readline()
         if (equals(answer, meaning)):
             print("Your'e answer is correct!")
         else:
-            print(f"Wrong! The correct answer is: {meaning}")
+            print(f'Wrong! The correct answer is: {meaning}')
 
 def equals(first, second):
     return first.lower().strip() == second.lower().strip()
