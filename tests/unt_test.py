506042b34f82f04e44db0647ec1e13b9861cc9c6
amitkummer <amit.kummer@gmail.com>
2020-12-26 15:18:23 +0200

    Add couple more tests

--- tests/unt_test.py
+++ tests/unt_test.py
@@ -1,5 +1,15 @@
+from pathlib import Path
+
 import memorizer
 
 
-def test_equals():
+def test_equals_diff_case():
     assert memorizer.equals("A", "a")
+
+def test_eqals_has_spaces():
+    assert memorizer.equals("a", " a ")
+
+def test_sampled_size():
+    p = Path('.').joinpath('data').joinpath('test.json')
+    result = memorizer.sampleWordsFromFile(p, 2)
+    assert len(result) == 2
