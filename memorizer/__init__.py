f0cd54ae57b428a9c1192ddd90605795045918d8
amitkummer <amit.kummer@gmail.com>
2021-01-02 19:04:17 +0200

    Pimp the cli with rich
    Noteable additions:
    -  If cound > count of words in file, set count = counf of words in file and omit a warning.
    - Added exit() command.
    - Added info about ignoring white spaces and letter's case.
    - Added info about cycling of wrong words until correct input.
    - Pimped colors of existing output.

--- memorizer/__init__.py
+++ memorizer/__init__.py
@@ -3,6 +3,12 @@ from pathlib import Path
 import random
 import json
 import fileinput
+import sys
+
+from rich.console import Console
+from rich.prompt import Prompt
+
+console = Console() 
 
 def run():
     parser = ArgumentParser(prog='memorizer')
@@ -17,28 +23,40 @@ def run():
 def sampleWordsFromFile(path, sampleCount):
     with path.open(encoding='UTF-8') as wordsFile:
         words = json.load(wordsFile)
+        if (len(words) < sampleCount):
+            sampleCount = len(words)
+            console.print('Warning: count argument is bigger than' +
+            'words count in file. All words in file are picked.',
+            style="yellow")
         return random.sample(words, sampleCount)
+            
 
 def startGame(words):
-    print('Game is starting! Write your answer and press enter.')
+    console.print('Game is starting! Write your answer and press enter.',
+    style="green")
+    console.print('Type exit() to exit the game.', style='green')
+    console.print("Letter's case and spaces are ignored.", style='green')
+    console.print("Wrong words will be cycled until correct input.", style='green')
     mistakes = iterate(words)
     while (len(mistakes) != 0):
         mistakes = iterate(mistakes)
 
 def iterate(words):
-    console = fileinput.input('-')
+    input = fileinput.input('-')
     mistakes = []
     for word in words:
         german = word['word']
         meaning = word['meaning']
-        print(f'What is the English word for the word: {german}?')
-        answer = console.readline()
+        answer = Prompt.ask(f'What is the English word for the word: [cyan]{german}?')
+        if (equals(answer, "exit()")):
+            sys.exit(0)
         if (equals(answer, meaning)):
-            print("Your'e answer is correct!")
+            console.print("Your'e answer is correct!", style='green')
         else:
             mistakes.append(word)
-            print(f'Wrong! The correct answer is: {meaning}')
-    console.close()
+            console.print(f'Wrong! The correct answer is: [bold]{meaning}',
+            style='red')
+    input.close()
     return mistakes
 
 def equals(first, second):
