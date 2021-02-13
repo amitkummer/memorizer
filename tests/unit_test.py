5eb042de45aff4525de918d096bd1f8fc4530e9b
amitkummer <amit.kummer@gmail.com>
2021-02-13 13:06:56 +0200

    Add more word and fix typo in test name

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -5,7 +5,7 @@ import memorizer
 def test_equals_diff_case():
     assert memorizer.equals("A", "a")
 
-def test_eqals_has_spaces():
+def test_equals_has_spaces():
     assert memorizer.equals("a", " a ")
 
 def test_sampled_size_from_file():
